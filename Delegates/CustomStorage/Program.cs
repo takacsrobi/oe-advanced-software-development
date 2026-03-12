ff8818506b853cc1c981d9a0a1450bb8678f3b6e
Takács Róbert <100440223+takacsrobi@users.noreply.github.com>
2026-03-12 19:50:29 +0100

    Add files via upload

---
+++ Delegates/CustomStorage/Program.cs
@@ -0,0 +1,31 @@
+﻿//Módosítsd a Storage osztály delegáltjait beépített delegáltakra
+//A külső függvényeket Lambda kifejezéssel add át!
+
+using CustomStorage;
+
+//void WriteOut(string item)
+//{
+//    Console.WriteLine(item);
+//}
+
+//string Upper(string item)
+//{
+//    return item.ToUpper();
+//}
+
+//string Format(string item)
+//{
+//    return "--- " + item + " ---";
+//}
+
+
+Storage<string> st = new Storage<string>(5);
+st.AddTransformer(t => t.ToUpper());
+st.AddTransformer(t => "--- " + t + " ---");
+
+st.Add("Jack");
+st.Add("Kate");
+st.Add("John");
+st.Add("Michael");
+
+st.Traverse(Console.WriteLine);
